bf1f743574a598ad902439dcb7f19be16508fbc8
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-07-28 02:03:02 +0200

    The bot is fully functional

--- elmundo_bot/app.py
+++ elmundo_bot/app.py
@@ -7,49 +7,31 @@ import logging
 from telebot import types
 import re
 import os
+from article import Article
 import feedparser
-from bs4 import BeautifulSoup
-import html2text
 
 API_TOKEN = os.getenv("ELMUNDO_BOT")
 elmundo_rss = "http://estaticos.elmundo.es/elmundo/rss/{endpoint}.xml"
 
-class Article:
-    def __init__(self, title, content, link):
-        html_cleaned = clean_content(content)
-        print(html_cleaned)
-        self.content = parse_to_markdown(html_cleaned)
-        print(self.content)
-        self.link = link
-        self.title = title
-        
-    def __repr__(self):
-        return '*{title}*\n{content}... [Leer más]({link})'.format(content=self.content,
-                                                                                    link=self.link,
-                                                                                    title=self.title)
+welcome_message = "*Hola, soy ElMundoBot*, tu bot de noticias\n" \
+                  "Para poder usarme, debes llamarme en la línea de ordenes con:\n" \
+                  "@elmundo\_inlinebot _<nombre de la sección_> para que te busque las últimas noticias.\n" \
+                  "Las secciones que dispongo para enseñarte son: \n" \
+                  "españa\n" \
+                  "Espero serte de gran ayuda! *:)*\n" \
+                  "Mi código fuente está en [este repositorio](https://github.com/JCepedaVillamayor/agilecr-lightning-talks)"
 
-def clean_content(content):
-    content_fixed = fix_unclosed_html(content)
-    content_cleaned = remove_useless_tags(content_fixed)
-    return content_cleaned
-
-def fix_unclosed_html(html):
-    soup = BeautifulSoup(html)
-    result = ""
-    for children in soup.body:
-        result += str(children)
-    return result
-
-def remove_useless_tags(html):
-    html = html.replace("<p>","").replace("</p>","")
-    return html
-
-def parse_to_markdown(html):
-    content_markdownized = html2text.html2text(html)
-    return content_markdownized
+asda = ""\
+                  "Para poder usarme, debes llamarme en la línea de ordenes con: \n"\
+                  " \@elmundo_inlinebot nombre de la sección para que te busque las últimas noticias.\n"\
+                  "Las secciones que dispongo para enseñarte son: \n"\
+                  "españa"\
+                  "Espero serte de gran ayuda! :)\n"\
+                  "Mi código fuente está en [este repositorio](https://github.com/JCepedaVillamayor/agilecr-lightning-talks)"
+print(welcome_message)
 
 bot = telebot.TeleBot(API_TOKEN)
-telebot.logger.setLevel(logging.DEBUG)
+#telebot.logger.setLevel(logging.DEBUG)
 
 @bot.inline_handler(lambda query: re.match("españa",query.query, re.IGNORECASE))
 def query_spain_elmundo(inline_query):
@@ -65,16 +47,25 @@ def query_spain_elmundo(inline_query):
     except Exception as e:
         print(e)
 
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, welcome_message, parse_mode="Markdown")
+
 def obtain_posts(endpoint):
-    rss = feedparser.parse(elmundo_rss.format(endpoint=endpoint))
     articles = []
+    try:
+        rss = feedparser.parse(elmundo_rss.format(endpoint=endpoint))
+    except Exception as e:
+        article = Article("La pagina de El Mundo no funciona correctamente","Disculpen las molestias","")
+        return [article]
+    
     for item in rss.entries:
         try:
             article = Article(item.title, item.content[0]['value'], item.link)
-            print(article)
             articles.append(article)
         except Exception as e:
             print(e)
+            
     return articles
 
 def main_loop():
---
+++ elmundo_bot/article.py
@@ -0,0 +1,15 @@
+from utils import clean_content, fix_unclosed_html, remove_useless_tags, parse_to_markdown
+
+class Article:
+    def __init__(self, title, content, link):
+        html_cleaned = clean_content(content)
+        print(html_cleaned)
+        self.content = parse_to_markdown(html_cleaned)
+        print(self.content)
+        self.link = link
+        self.title = title
+        
+    def __repr__(self):
+        return '*{title}*\n{content}... [Leer más]({link})'.format(content=self.content,
+                                                                                    link=self.link,
+                                                                                    title=self.title)
